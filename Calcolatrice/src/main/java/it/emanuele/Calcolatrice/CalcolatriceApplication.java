ebe6efbbf5dbb755fe916dc909ddd5bd3e46d6b0
EmanueleDeCicco97 <115298739+EmanueleDeCicco97@users.noreply.github.com>
2022-10-27 21:10:13 +0200

    Add files via upload

---
+++ Calcolatrice/src/main/java/it/emanuele/Calcolatrice/CalcolatriceApplication.java
@@ -0,0 +1,12 @@
+package it.emanuele.Calcolatrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcolatriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcolatriceApplication.class, args);
+	}
+}
